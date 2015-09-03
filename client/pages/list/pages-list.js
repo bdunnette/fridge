9a162740902cb3cef62d3fba49e25ca4aeefcc31
Brian Dunnette <brian@dunnette.us>
2015-09-02 19:37:16 -0500

    okay, re-adding textangular tool example?

--- client/pages/list/pages-list.js
+++ client/pages/list/pages-list.js
@@ -3,4 +3,19 @@ angular
     function ($scope, $meteor) {
       $scope.pages = $meteor.collection(Pages);
     }
-  ]);
+  ])
+  .config(function($provide){
+    $provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions){
+        // $delegate is the taOptions we are decorating
+        // register the tool with textAngular
+        taRegisterTool('colourRed', {
+            iconclass: "fa fa-square red",
+            action: function(){
+                this.$editor().wrapSelection('forecolor', 'red');
+            }
+        });
+        // add the button to the default toolbar definition
+        taOptions.toolbar[1].push('colourRed');
+        return taOptions;
+    }]);
+});
